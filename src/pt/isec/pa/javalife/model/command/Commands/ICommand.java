e7032fa7e318c7bce6074d637bd2dd6f456cca6f
joaod <joaoduartepereirapinto@gmail.com>
2024-04-20 18:23:35 +0100

    epá, se não houverem mais parâmetros a mudar no ecossistema já não devem ser necessários mais comandos

--- src/pt/isec/pa/javalife/model/command/Commands/ICommand.java
+++ src/pt/isec/pa/javalife/model/command/Commands/ICommand.java
@@ -1,9 +1,7 @@
 package pt.isec.pa.javalife.model.command.Commands;
 
 public interface ICommand {
-
     boolean execute();
-
     boolean undo();
 
     boolean isSuccessful();
--- src/pt/isec/pa/javalife/model/command/ICommand.java
+++
@@ -1,6 +0,0 @@
-package pt.isec.pa.javalife.model.command;
-
-public interface ICommand {
-    boolean execute();
-    boolean undo();
-}
